a1f82e6aee4e3daaf0042ca7c9ba38f35658e00d
student <student@virtualbox>
2016-09-28 15:33:15 +0200

    initial commit

---
+++ src/main/java/helloproject/helloproject/App.java
@@ -0,0 +1,16 @@
+package helloproject.helloproject;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ArrayList<Object> newArray=new ArrayList<Object>();
+    	
+    }
+}
